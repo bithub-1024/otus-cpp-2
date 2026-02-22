62b37b17844922b676fe73ff3eed7a44f799a46a
bithub.1024 <bithub.1024@gmail.com>
2026-02-22 10:50:23 +0300

    Revert "remove all files for review"
    This reverts commit c11b80bde680d35d4de1d324c145a9e84a287018.
    
    For review

---
+++ src/ip-filter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "filter_ip.hpp"
+
+int main()
+{
+    std::vector<ip_t> pool;
+
+    load_pool_ip(pool); /*← как было, оставляешь*/
+
+    sort_pool_ip(pool);
+
+    for (const auto& ip : filter_ip(pool))
+        std::cout << ip << '\n';
+
+    for (const auto& ip : filter_ip(pool, 1))
+        std::cout << ip << '\n';
+
+    for (const auto& ip : filter_ip(pool, 46, 70))
+        std::cout << ip << '\n';
+
+    for (const auto& ip : filter_any(pool, 46))
+        std::cout << ip << '\n';
+}
+
